3cad90d22191b66e5ffd651b1d859912995b32fb
Kholid Irfangi <kholidirfangi394@gmail.com>
2025-02-03 07:08:12 +0700

    fix event section

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -42,36 +42,41 @@ const Event = () => {
       <div className="h-1 w-20 bg-sky-50 rounded-lg mx-auto mb-10"></div>
       <div className="px-5 w-lg">
         <h1 className="text-center font-great-vibes text-3xl my-20 text-white">
-          <div data-aos="fade-right" className='-ml-5'>Wedding</div>
-          <div data-aos="fade-left" className='ml-20'> Event </div>
+          <div data-aos="fade-right" className="-ml-5">
+            Wedding
+          </div>
+          <div data-aos="fade-left" className="ml-20">
+            {' '}
+            Event{' '}
+          </div>
         </h1>
         {/* Akad Nikah */}
         <div
           data-aos="fade-up"
-          className="mb-14 rounded-tr-[150px] overflow-hidden shadow-lg"
+          className="mb-14 rounded-tr-[180px] overflow-hidden shadow-lg"
         >
           <div>
             {/* Container untuk override margin */}
-            <Slider {...landscapeSettings1} className="w-lg">
+            <Slider {...landscapeSettings1} className="w-full">
               {landscapeImages.map((img, index) => (
                 <div key={index} className="h-64">
                   <img
                     src={img}
-                    className="w-lg h-lg object-cover"
+                    className="w-full h-full object-cover"
                     lazyload="true"
                   />
                 </div>
               ))}
             </Slider>
           </div>
-          <div className="flex w-lg -mt-2">
-            <div className="relative w-1/4 bg-sky-700 text-sky-50 text-lg -z-10">
-              <p className="-rotate-90  text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap origin-center text-2xl tracking-widest">
+          <div className="flex w-full -mt-2">
+            <div className="relative w-1/4 bg-sky-700 text-white text-lg">
+              <p className="-rotate-90  text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap origin-center tracking-widest text-2xl">
                 Akad Nikah
               </p>
             </div>
-            <div className="w-3/4 bg-sky-50">
-              <div className="flex gap-5 justify-center items-center mt-10 mb-5 text-sky-950">
+            <div className="w-3/4 bg-white">
+              <div className="flex gap-5 justify-center items-center mt-10 mb-5 text-sky-900">
                 <div className="text-5xl">30</div>
                 <div className="text-xl">
                   Kamis <br />
@@ -81,9 +86,9 @@ const Event = () => {
               </div>
               <div
                 data-aos="fade-left"
-                className="h-[2px] bg-sky-950 w-11/12 mx-auto rounded-lg"
+                className="h-[2px] bg-sky-700 w-11/12 mx-auto rounded-full"
               ></div>
-              <div className="px-5 flex flex-col gap-2 my-3 text-sky-950">
+              <div className="px-5 flex flex-col gap-2 my-3 text-sky-900">
                 <p>
                   <FontAwesomeIcon icon={faClock} className="text-sm mr-2" />
                   08.00 WIB
@@ -93,7 +98,7 @@ const Event = () => {
                 <p>Jl. Cilandak KKO, Jakarta Selatan</p>
                 <a
                   href=""
-                  className="inline-block p-1 border-2 w-36 text-center rounded-lg text-sm hover:bg-sky-700 hover:text-sky-50 my-5"
+                  className="inline-block p-1 border-2 w-36 text-center rounded-lg text-sm hover:bg-sky-700 hover:text-white my-5"
                 >
                   Google Maps
                 </a>
@@ -105,42 +110,42 @@ const Event = () => {
         {/* Resepsi */}
         <div
           data-aos="fade-up"
-          className="mb-14 rounded-tl-[150px] overflow-hidden shadow-lg"
+          className="mb-14 rounded-tl-[180px] overflow-hidden shadow-lg"
         >
           <div>
             {/* Container untuk override margin */}
-            <Slider {...landscapeSettings2} className="w-lg">
+            <Slider {...landscapeSettings2} className="w-full">
               {landscapeImages.map((img, index) => (
                 <div key={index} className="h-64">
                   <img
                     src={img}
-                    className="w-lg h-lg object-cover"
+                    className="w-full h-full object-cover"
                     lazyload="true"
                   />
                 </div>
               ))}
             </Slider>
           </div>
-          <div className="flex flex-row-reverse w-lg -mt-2">
-            <div className="relative w-1/4 bg-sky-700 text-sky-50 text-lg -z-10">
-              <p className="rotate-90  text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap origin-center text-2xl tracking-widest">
+          <div className="flex flex-row-reverse w-full -mt-2">
+            <div className="relative w-1/4 bg-sky-700 text-white text-lg">
+              <p className="rotate-90  text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap origin-center tracking-widest text-2xl">
                 Resepsi
               </p>
             </div>
             <div className="w-3/4 bg-white">
-              <div className="flex gap-5 justify-center items-center mt-10 mb-5 text-sky-950">
-                <div className="text-5xl">31</div>
+              <div className="flex gap-5 justify-center items-center mt-10 mb-5 text-sky-900">
+                <div className="text-5xl">30</div>
                 <div className="text-xl">
-                  Jumat <br />
+                  Kamis <br />
                   Desember
                   <br /> 2025
                 </div>
               </div>
               <div
-                data-aos="fade-right"
-                className="h-[2px] bg-sky-950 w-11/12 mx-auto rounded-lg"
+                data-aos="fade-left"
+                className="h-[2px] bg-sky-700 w-11/12 mx-auto rounded-full"
               ></div>
-              <div className="px-5 flex flex-col gap-2 my-3 text-sky-950">
+              <div className="text-sky-900 px-5 flex flex-col gap-2 my-3">
                 <p>
                   <FontAwesomeIcon icon={faClock} className="text-sm mr-2" />
                   08.00 WIB
@@ -160,44 +165,45 @@ const Event = () => {
         </div>
 
         {/* Ngunduh Mantu */}
+
         <div
           data-aos="fade-up"
-          className="mb-14 rounded-tr-[150px] overflow-hidden shadow-lg"
+          className="mb-14 rounded-tr-[180px] overflow-hidden shadow-lg"
         >
           <div>
             {/* Container untuk override margin */}
-            <Slider {...landscapeSettings1} className="w-lg">
+            <Slider {...landscapeSettings1} className="w-full">
               {landscapeImages.map((img, index) => (
                 <div key={index} className="h-64">
                   <img
                     src={img}
-                    className="w-lg h-lg object-cover"
+                    className="w-full h-full object-cover"
                     lazyload="true"
                   />
                 </div>
               ))}
             </Slider>
           </div>
-          <div className="flex w-lg -mt-2">
-            <div className="relative w-1/4 bg-sky-700 text-white text-lg -z-10">
-              <p className="-rotate-90  text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap origin-center text-2xl tracking-widest">
+          <div className="flex w-full -mt-2">
+            <div className="relative w-1/4 bg-sky-700 text-white text-lg">
+              <p className="-rotate-90  text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap origin-center tracking-widest text-2xl">
                 Ngunduh Mantu
               </p>
             </div>
             <div className="w-3/4 bg-white">
-              <div className="flex gap-5 justify-center items-center mt-10 mb-5 text-sky-950">
-                <div className="text-5xl">31</div>
+              <div className="flex gap-5 justify-center items-center mt-10 mb-5 text-sky-900">
+                <div className="text-5xl">30</div>
                 <div className="text-xl">
-                  Jumat <br />
+                  Kamis <br />
                   Desember
                   <br /> 2025
                 </div>
               </div>
               <div
                 data-aos="fade-left"
-                className="h-[2px] bg-sky-950 w-11/12 mx-auto rounded-lg"
+                className="h-[2px] bg-sky-700 w-11/12 mx-auto rounded-full"
               ></div>
-              <div className="px-5 flex flex-col gap-2 my-3 text-sky-950">
+              <div className="px-5 flex flex-col gap-2 my-3 text-sky-900">
                 <p>
                   <FontAwesomeIcon icon={faClock} className="text-sm mr-2" />
                   08.00 WIB
